f1b99ef55afa6f9c803cba81865d2b99e37403b3
Micah Whitacre <micah.whitacre@cerner.com>
2015-04-03 13:18:36 -0500

    Added another action that should result in multiple repeat work

---
+++ src/main/scala/com/mkwhitacre/spark/SparkMultipleWritesNoActions.scala
@@ -0,0 +1,22 @@
+package com.mkwhitacre.spark
+
+import org.apache.spark.{SparkConf, SparkContext}
+import org.apache.spark.SparkContext._
+
+object SparkMultipleWritesNoAction {
+  def main(args: Array[String]) {
+    val sc = new SparkContext(new SparkConf().setAppName("Spark Counts Multiple Writes No Action"))
+    val threshold = args(1).toInt
+
+    // split each document into words
+    val tokenized = sc.textFile(args(0)).flatMap(_.split(" "))
+
+    // count characters
+    val charCounts = tokenized.flatMap(_.toCharArray).map((_, 1))
+
+    charCounts.saveAsTextFile("/tmp/spark/char_count_out1")
+    charCounts.saveAsTextFile("/tmp/spark/char_count_out2")
+
+//    System.out.println(charCounts.collect().mkString(", "))
+  }
+}
