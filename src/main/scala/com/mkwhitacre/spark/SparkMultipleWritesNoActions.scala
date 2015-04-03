f6c06de89ad74badab882602c39c446aa93983d6
Micah Whitacre <micah.whitacre@cerner.com>
2015-04-03 14:43:28 -0500

    make function more expensive

--- src/main/scala/com/mkwhitacre/spark/SparkMultipleWritesNoActions.scala
+++ src/main/scala/com/mkwhitacre/spark/SparkMultipleWritesNoActions.scala
@@ -1,5 +1,8 @@
 package com.mkwhitacre.spark
 
+import java.lang.Thread.sleep
+
+import org.apache.spark.rdd.RDD
 import org.apache.spark.{SparkConf, SparkContext}
 
 object SparkMultipleWritesNoAction {
@@ -8,14 +11,21 @@ object SparkMultipleWritesNoAction {
     val threshold = args(1).toInt
 
     // split each document into words
-    val tokenized = sc.textFile(args(0)).flatMap(_.split(" "))
+    val tokenized: RDD[String] = sc.textFile(args(0)).flatMap(_.split(" "))
 
     // count characters
-    val charCounts = tokenized.flatMap(_.toCharArray).map((_, 1))
+    val chars = tokenized.flatMap(_.toCharArray)
+    val charCounts: RDD[(Char, Int)] =  chars.map(charCountSleep(_))
 
     charCounts.saveAsTextFile("/tmp/spark/char_count_out1")
     charCounts.saveAsTextFile("/tmp/spark/char_count_out2")
 
 //    System.out.println(charCounts.collect().mkString(", "))
   }
+
+  def charCountSleep(c: Char): (Char, Int) = {
+    Thread sleep 5000
+    (c, 1)
+  }
+
 }
